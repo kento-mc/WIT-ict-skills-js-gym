cc1e16350bdabfc52a2c4fb38924dc2669993370
Glitch (wit-hdip-comp-sci-2019-glitchgym) <none>
2019-07-19 13:00:24 +0000

    🎽🏕 Checkpoint ./controllers/accounts.js:4725629/1717 ./routes.js:4725629/829

--- routes.js
+++ routes.js
@@ -1,13 +1,20 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const accounts = require ("./controllers/accounts.js");
-const dashboard = require("./controllers/dashboard.js");
-const about = require("./controllers/about.js");
+const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
+const playlist = require('./controllers/playlist.js');
+const accounts = require('./controllers/accounts.js');
 
-router.get("/", accounts.index);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
 

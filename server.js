067573187cf20ffbdd1b6f13518cf08ee443baf9
Glitch (wit-hdip-comp-sci-2019-glitchgym) <none>
2019-08-28 22:44:44 +0100

    finally got images displaying. Had to implement the solution from this stackoverflow page: https://stackoverflow.com/questions/50217609/handlebars-cant-display-images. Was a problem in the server.js file

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cookieParser());
 const exphbs = require("express-handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use(express.static(__dirname+'/public')); // Used https://stackoverflow.com/questions/50217609/handlebars-cant-display-images
 app.use(fileUpload());
 app.engine(
   ".hbs",
